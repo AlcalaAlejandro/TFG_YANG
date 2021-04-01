34cb15deb47ebd52847b3029d3cb5625cfb366e1
Alejandro Alcalá Álvarez <alejandro.alcala@estudiante.uam.es>
2021-04-01 10:03:43 +0200

    Subido los cambios a la rama Volta.

--- Nueva_VesionIETF/Network_Slice_Orchestrator.py
+++ Nueva_VesionIETF/Network_Slice_Orchestrator.py
@@ -2,9 +2,11 @@ import Candidate
 import Running
 import json
 import time
+import os
 
 def checkNetworkSlice ():
 	running = True
+	node_json = {}
 
 	if getCandidateOrchestrator == 0:
 		print("Error in checkNetworkSlice.")
@@ -16,14 +18,35 @@ def checkNetworkSlice ():
 
 	if "network" in networkSlice["ietf-network:networks"]:
 		for network in networkSlice["ietf-network:networks"]["network"]:
+			nodes_id = []
 			print("Network-id: "+ str(network["network-id"]))
 			print("Nodes of Network:")
 			if "node" in network:
+				port = {}
+				node_json.clear()
 				for node in network["node"]:
 					print("\t Node id: "+ str(node["node-id"]))
 					if "ietf-network-topology:termination-point" in node:
 						for tp in node["ietf-network-topology:termination-point"]:
 							print("\t Termination Point id: "+ str(tp["tp-id"]))
+							nodes_id.append(str(tp["tp-id"]))
+					for n in nodes_id:
+						if os.path.exists(str(n)) == False:
+							os.mkdir(str(n))
+						node_json["device_id"] = str(n)
+						node_json["va_log_level"] = "INFO"
+						node_json["nevi_log_leve"] = "INFO"
+						node_json["ports"] = []
+						port["autoneg"] = False
+						port["mtu"] = 1500
+						port["admin_status"] = "up"
+						port["ports"] = 1
+						port["ports"] = ["10G", "1G"]
+						node_json["ports"].append(port)
+
+
+						with open(str(n)+'/device-setup.json', 'w') as file:
+							json.dump(node_json, file, indent = 4)
 			else:
 				running = False
 
